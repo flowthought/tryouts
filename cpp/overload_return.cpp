12a374784e97a17e65c5d34e96996d61c12520f2
Kalpak Tapas <katapas@microsoft.com>
2017-11-05 21:31:00 +0530

    cpp/overload_return

---
+++ cpp/overload_return.cpp
@@ -0,0 +1,18 @@
+// Requirement: T switcher(bool) returns type A if true is passed, B if false is passed
+//
+
+struct switcher
+{
+
+    template<typename T>
+    T Get(bool b)
+    {
+        return T(0);
+    }
+}
+
+void main()
+{
+
+    return 0;
+}

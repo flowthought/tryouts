d9aced785cb444ee4a04efe3a853b6a2a1824cbc
Kalpak Tapas <kalpak1707@gmail.com>
2018-03-14 03:57:48 +0530

    More csv op

--- python/csvop.py
+++ python/csvop.py
@@ -1,3 +1,4 @@
+# Read a csv into memory
 from collections import OrderedDict
 
 with open('baseline.csv') as csvfile:
@@ -7,3 +8,7 @@ with open('baseline.csv') as csvfile:
             records.append(row)
 
 # Records is a list of ordered dict
+
+x = [1, 2, 3]
+y = [4, 5, 6]
+sum_x_y = [i + j for (i, j) in zip(x, y)]

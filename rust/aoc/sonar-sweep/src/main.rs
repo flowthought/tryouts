327eecf719ba0d4146b865d803aaaa67c3a8f591
Kalpak Tapas <kalpak1707@gmail.com>
2022-05-28 21:54:47 +0530

    Sonar sweep #1 success

--- rust/aoc/sonar-sweep/src/main.rs
+++ rust/aoc/sonar-sweep/src/main.rs
@@ -1,9 +1,38 @@
+use std::io;
+
 fn main() {
-    // let world: &'static str = "prithvi";
-    let num = 238479238;
-    println!("Hello, {}! -Stranger{}", foo("bar"), num);
+  let mut buffer = String::new();
+  let mut inc_count: i32 = 0;
+  let mut prev: i32 = std::i32::MAX;
+  
+  loop {
+    match io::stdin().read_line(&mut buffer) {
+      Ok(0) => {
+        break;
+      }
+      Ok(_) => {
+        // println!("{buffer:?}");
+        match buffer.trim_end().parse::<i32>() {
+          Ok(curr) => {
+            if curr > prev {
+              inc_count += 1;
+            }
+            prev = curr;
+          }
+          Err(error) => {
+            println!("Parse error: {error}");
+          }
+        }
+      }
+      Err(error) => {
+        println!("error: {error}");
+        break;
+      }
+    }
+    buffer.clear();
+  }
+  
+  println!("{inc_count} measurements increased");
+
 }
 
-fn foo(_x: &'static str) -> &'static str {
-    _x
-}
